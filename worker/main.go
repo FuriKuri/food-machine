1b9981bd35cf338914e3266c54daad2925ece6a9
Theo Pack <theo.pack@cologne-intelligence.de>
2018-07-06 09:53:46 +0200

    Generate fruits

--- worker/main.go
+++ worker/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/rand"
+	"fmt"
 	"log"
 	"time"
 
@@ -13,6 +15,8 @@ const (
 	address = "localhost:50051"
 )
 
+var machineID = pseudoUUID()
+
 func main() {
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
@@ -21,11 +25,26 @@ func main() {
 	defer conn.Close()
 	c := pb.NewFruitCollectorClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	_, err = c.Deliver(ctx, &pb.Fruit{Name: "apple", Worker: "1"})
-	if err != nil {
-		log.Fatalf("could not fruit: %v", err)
+	ticker := time.NewTicker(500 * time.Millisecond)
+	for t := range ticker.C {
+		fmt.Println("Tick at", t)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		_, err = c.Deliver(ctx, &pb.Fruit{Name: Pick(), Worker: machineID})
+		if err != nil {
+			log.Fatalf("could not fruit: %v", err)
+		}
 	}
-	log.Printf("OK")
+	log.Printf("FINISHED")
+}
+
+func pseudoUUID() string {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return ""
+	}
+
+	return fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 }
